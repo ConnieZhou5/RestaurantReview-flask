f789dd940e7f74c601785435e5089a63e94068c8
Connie Zhou <conniestark@Connies-MacBook-Pro-2.local>
2023-03-19 00:25:45 -0400

    implemented models in models.py and route index and route details.

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+from datetime import datetime
+
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+
+app = Flask(__name__, static_folder='static')
+csrf = CSRFProtect(app)
+
+# WEBSITE_HOSTNAME exists only in production environment
+if 'WEBSITE_HOSTNAME' not in os.environ:
+    # local development, where we'll use environment variables
+    print("Loading config.development and environment variables from .env file.")
+    app.config.from_object('azureproject.development')
+else:
+    # production
+    print("Loading config.production.")
+    app.config.from_object('azureproject.production')
+
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+# Initialize the database connection
+db = SQLAlchemy(app)
+
+# Enable Flask-Migrate commands "flask db init/migrate/upgrade" to work
+migrate = Migrate(app, db)
+
+# The import must be done after db initialization due to circular import issue
+from models import Restaurant, Review
+
+@app.route('/', methods=['GET'])
+def index():
+    print('Request for index page received')
+    restaurants = Restaurant.query.all()
+    return render_template('index.html', restaurants=restaurants)
+
+@app.route('/<int:id>', methods=['GET'])
+def details(id):
+    restaurant = Restaurant.query.where(Restaurant.id == id).first()
+    reviews = Review.query.where(Review.restaurant == id)
+    return render_template('details.html', restaurant=restaurant, reviews=reviews)
